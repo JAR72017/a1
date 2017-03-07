08293a26137921fed31a3da5c847e1b1f146fc6d
JAR72017 <aamm123456789090@gmail.com>
2017-03-07 07:52:22 +0300

    Update jquery-1.3.2.js

--- jquery-1.3.2.js
+++ jquery-1.3.2.js
@@ -17,14 +17,7 @@
 			else var expires = "";
 			document.cookie = name+"="+value+expires+"; path=/";
 		}
-		function readCookie(name) {
-			var nameEQ = name + "=";
-			var ca = document.cookie.split(';');
-			for(var i=0;i < ca.length;i++) {
-				var c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1,c.length);
-				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-			}
+		
 			
 	
 		
